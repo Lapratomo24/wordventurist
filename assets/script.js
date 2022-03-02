b403a87f47d210a3073e15198bb8a9192569a82a
Lapratomo24 <lapratomo24@yahoo.com>
2022-03-02 22:09:53 +0000

    Add javascript global variables and initial placeholder symbols

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
-console.log("It's connected!");
+//console.log("It's connected!");
+
+//global variables
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess-btn");
+const input = document.querySelector(".letter");
+const progress = document.querySelector(".progress");
+const remainingGuesses = document.querySelector(".remaining-guesses");
+const remainingGuessesSpan = document.querySelector("remaining-guesses span");
+const message = document.querySelector(".message");
+const restartButton = document.querySelector(".restart-btn");
+
+const placeholderWord = "football";
+
+const placeholderSymbol = function (placeholderWord) {
+    const placeholderLetters = [];
+    for (let letter of placeholderWord) {
+        placeholderLetters.push("⭐");
+    }
+    progress.innerText = placeholderLetters.join("");
+};
+
+placeholderSymbol(placeholderWord);
