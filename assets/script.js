42f2bdd7bb7c52a4af6d2b98bd4b311e51039f16
Lapratomo24 <lapratomo24@yahoo.com>
2022-03-02 22:59:26 +0000

    Add function to validate input

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,8 @@ const restartButton = document.querySelector(".restart-btn");
 
 const placeholderWord = "football";
 
+
+//set up star symbols as placeholders
 const placeholderSymbol = function (placeholderWord) {
     const placeholderLetters = [];
     for (let letter of placeholderWord) {
@@ -21,3 +23,26 @@ const placeholderSymbol = function (placeholderWord) {
 };
 
 placeholderSymbol(placeholderWord);
+
+
+//add event listener to capture letter input
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const inputValue = input.value;
+    console.log(inputValue);
+    input.value="";
+});
+
+//add function to validate input
+const inputValidation = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please input a letter of your choice!"
+    } else if (input.length > 1) {
+        message.innerText = "Only one letter for every turn!"
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Hint: A to Z, no numbers and symbols!"
+    } else {
+        return input;
+    }
+};
